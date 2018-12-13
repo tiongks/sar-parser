097efa2e7e75c7bf2f7ee7a4e1cfaaf96b63599d
Eric Tiongson <eric@tiongson.ph>
2018-12-13 12:13:04 -0700

    changed graph style to stacked bar, showed details

--- basic-parser.R
+++ basic-parser.R
@@ -1,17 +1,18 @@
-library("ggplot2")
-library("dplyr")
+library(ggplot2)
+library(dplyr)
+library(reshape2)
 
 parse_sar_file <- function(filename) {
   sar_raw <- read.table(filename, skip = 1, header = TRUE, fill = TRUE)
   sar_filtered <- sar_raw[sar_raw$CPU == "all",]
   # sar_filtered <- sar_raw[sar_raw$CPU %in% (0:23),]
   rm(sar_raw)
-  sar_header <- read.table(filename, nrows = 1, header=FALSE)
-  sar_date <- as.character(sar_header[1,4])
+  sar_date <- as.character(read.table(filename, nrows = 1, header=FALSE)[1,4])
   print(paste(filename, sar_date))
-  rm(sar_header)
-  sar_cpu <- sar_filtered[,c(1,3,4,6,7)]
-  names(sar_cpu) <- c("time", "cpu","usr", "sys", "iowait")
+  sar_cpu <- sar_filtered[,c(1,2,4,6,7)]
+  
+  names(sar_cpu) <- c("time", "ampm","usr", "sys", "iowait")
+  sar_cpu$timestamp <- paste(sar_cpu$time, sar_cpu$ampm)
   sar_cpu$usr <- as.numeric(sub(" ", x = sar_cpu$usr, ""))
   sar_cpu$sys <- as.numeric(sub(" ", x = sar_cpu$sys, ""))
   sar_cpu$iowait <- as.numeric(sub(" ", x = sar_cpu$iowait, ""))
@@ -19,11 +20,13 @@ parse_sar_file <- function(filename) {
   output_csv <- paste("exported-", filename, ".csv", sep = "")
   output_png <- paste("exported-", filename, ".png", sep = "")
   write.csv(file=output_csv, x = sar_cpu)
-  ggplot(data =  sar_cpu) + geom_point() +
-    aes(x = time, y = total, color = cpu) +
+  s_g <- sar_cpu[,c(6,3,4,5)]
+  s_melted <- melt(sar_cpu[,c(6,3,4,5)], id="timestamp")
+  ggplot(data = s_melted, aes(timestamp, y=value)) + 
+    geom_bar(stat = "identity", aes(fill = variable)) +
     labs(title=sar_date, x = "time", y = "total") +
-    theme(axis.text.x = element_text(angle = 90, hjust = 1))
-  ggsave(output_png, width = 10, units = "in", limitsize = FALSE, scale = 1.5)
+    theme(axis.text.x = element_text(angle = 45, hjust = 1, size = 6))
+  ggsave(output_png, width = 10, units = "in", limitsize = FALSE, scale = 2)
   rm(sar_cpu)
 }
 
